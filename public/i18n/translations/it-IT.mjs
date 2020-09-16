c17d0138b078cdd3f180543227d9184150237621
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2020-09-16 13:07:28 +0200

    Italian grammar corrections + register IT locale

--- public/i18n/translations/it-IT.mjs
+++ public/i18n/translations/it-IT.mjs
@@ -11,7 +11,7 @@ export const translations = {
     'Is it outside or inside?': {translation: 'L\'evento si terrà all\'aperto o al chiuso?'},
     'outside': {translation: 'all\'aperto'},
     'inside': {translation: 'al chiuso'},
-    'Is it well ventilated?': {translation: 'Il luogo è ben ventillato?'},
+    'Is it well ventilated?': {translation: 'Il luogo è ben ventilato?'},
     'well ventilated': {translation: 'ben ventilato'},
     'poorly ventilated': {translation: 'ventilato male'},
     'Do all others near you wear masks?': {translation: 'Tutti portano la mascherina intorno a te?'},
@@ -20,16 +20,16 @@ export const translations = {
     'Are you there for a long time?': {translation: 'Resti là per un lungo periodo di tempo?'},
     'short time': {translation: 'per un breve periodo'},
     'prolonged time': {translation: 'per un lungo periodo'},
-    'Are people near you speaking?': {translation: 'Come parlano le persono intorno a te?'},
+    'Are people near you speaking?': {translation: 'Le persono intorno a te parlano?'},
     'silent': {translation: 'sono in silenzio'},
     'speaking': {translation: 'chiacchierano'},
     'shouting/singing': {translation: 'parlano ad alta voce o cantano'},
 
     /* Result */
     'Are you in danger?': {translation: 'Sei a rischio?'},
-    'Not sure yet. Give a few more answers.': {translation: 'Non lo sappiamo ancora. Rispondi ancora alcune domande.'},
+    'Not sure yet. Give a few more answers.': {translation: 'Non lo sappiamo ancora. Rispondi ancora ad alcune domande.'},
     'You are at manageable risk. Take care though.': {translation: 'Sei in una situazione con rischio gestibile. Stai attento però e nota se la situazione cambia.'},
-    'You are at medium risk. Keep your distance at all times from people outside your household.': {translation: 'Sei in una situazione con rischio medio. Tiene la distanza di sicurezza dalle persone con cui non convivi.'},
+    'You are at medium risk. Keep your distance at all times from people outside your household.': {translation: 'Sei in una situazione con rischio medio. Tieni la distanza di sicurezza dalle persone intorno a te.'},
     'You are at high risk. Avoid/get out of this situation to take care of yourself and others.': {translation: 'Sei in una situazione con rischio elevato. Evita questo posto o vai via per prenderti cura di te e degli altri.'},
 
     /* Table */
@@ -46,7 +46,7 @@ export const translations = {
     'Silent': {translation: 'In silenzio'},
     'Speaking': {translation: 'Chicchiera'},
     'Shouting, singing': {translation: 'Voce alta o canto'},
-    '* Borderline case that is highly dependent on quantitative definitions of distancing, number of individuals, and time of exposure': {translation: '* Situazione al limite che dipende tanto dalla definizione del distanziamento sociale, il numero delle persone e il periodo di esposizione.'},
+    '* Borderline case that is highly dependent on quantitative definitions of distancing, number of individuals, and time of exposure': {translation: '* Situazione al limite che dipende molto dalla definizione del distanziamento sociale, dal numero delle persone e dal periodo di esposizione.'},
 
     /* Footer */
     'Copyright': {translation: ''},
